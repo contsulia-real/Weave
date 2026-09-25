1b1357d939081d9b78804bb9519e83d27f3af32f
Contsulia <contsulia@gmail.com>
2026-09-25 20:33:57 +0800

    Fix Switch breakpoint prop typing

--- src/core/switch-types.ts
+++ src/core/switch-types.ts
@@ -1,20 +1,16 @@
-import type { ViewProps } from './view-types'
+import type { ViewCoreProps, ViewDynamicBreakpointProps } from './view-types'
 
 export type SwitchSize = 'small' | 'medium' | 'large'
 
-export type SwitchViewProps<
-  TBreakpoint extends string = never,
-> = Omit<
-  ViewProps<HTMLDivElement, TBreakpoint>,
+export type SwitchViewProps = Omit<
+  ViewCoreProps<HTMLDivElement>,
   'children' | 'checked'
->
+> & ViewDynamicBreakpointProps
 
-export interface SwitchProps<
-  TBreakpoint extends string = never,
-> {
+export interface SwitchProps {
   checked?: boolean
   defaultChecked?: boolean
   onChange?: (checked: boolean) => void
   size?: SwitchSize
-  viewProps?: SwitchViewProps<TBreakpoint>
+  viewProps?: SwitchViewProps
 }
