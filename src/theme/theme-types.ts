1b531bcc1ac85be83e7c16325e0d1182f8ac4a2d
Contsulia <contsulia@gmail.com>
2026-09-26 00:59:32 +0800

    Restore Switch theme types to stable baseline

--- src/theme/theme-types.ts
+++ src/theme/theme-types.ts
@@ -74,13 +74,9 @@ export interface SwitchThemeBase {
   background?: string
   radius?: ThemeScaleValue
   cursor?: string
-  trackShadow?: string
   thumbBackground?: string
   thumbRadius?: ThemeScaleValue
   thumbInset?: ThemeScaleValue
-  thumbShadow?: string
-  thumbHoverShadow?: string
-  thumbDragShrink?: number
   focusOutlineWidth?: ThemeScaleValue
   focusOutlineColor?: string
   focusOutlineStyle?: string
