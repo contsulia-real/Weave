0d1859a55ab921fbb3ed25169a0a3f191d8d39da
Contsulia <contsulia@gmail.com>
2026-09-25 19:31:27 +0800

    Allow custom breakpoints in Switch viewProps

--- src/core/switch-types.ts
+++ src/core/switch-types.ts
@@ -2,15 +2,19 @@ import type { ViewProps } from './view-types'
 
 export type SwitchSize = 'small' | 'medium' | 'large'
 
-export type SwitchViewProps = Omit<
-  ViewProps<HTMLDivElement>,
+export type SwitchViewProps<
+  TBreakpoint extends string = never,
+> = Omit<
+  ViewProps<HTMLDivElement, TBreakpoint>,
   'children' | 'checked'
 >
 
-export interface SwitchProps {
+export interface SwitchProps<
+  TBreakpoint extends string = never,
+> {
   checked?: boolean
   defaultChecked?: boolean
   onChange?: (checked: boolean) => void
   size?: SwitchSize
-  viewProps?: SwitchViewProps
+  viewProps?: SwitchViewProps<TBreakpoint>
 }
