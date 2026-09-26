1bfe0e2bf1e09216e4af4c09045bb1dc34728b99
Contsulia <contsulia@gmail.com>
2026-09-26 18:30:08 +0800

    Remove duplicate root text color declaration

--- src/renderers/dom/view-stylesheet.ts
+++ src/renderers/dom/view-stylesheet.ts
@@ -148,7 +148,6 @@ ${propertyRegistrationBlock()}
 :root {
   ${themeVariableDeclarations(defaultTheme)}
   color: var(--weave-color-text);
-  color: var(--weave-color-text);
   font-family: var(--weave-typography-family-body);
   font-size: var(--weave-typography-style-body-large-font-size);
   font-weight: var(--weave-typography-style-body-large-font-weight);
