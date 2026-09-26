4af43904e29d3dc70ffa31a211c681e94c415f6b
Contsulia <contsulia@gmail.com>
2026-09-26 12:51:54 +0800

    Make View baseline consume body-large typo

--- src/renderers/dom/view-stylesheet.ts
+++ src/renderers/dom/view-stylesheet.ts
@@ -152,10 +152,10 @@ ${propertyRegistrationBlock()}
 :where([data-weave-view]) {
   box-sizing: border-box;
   font-family: var(--weave-typography-family-body);
-  font-size: var(--weave-typography-size-medium);
-  font-weight: var(--weave-typography-weight-regular);
-  line-height: var(--weave-typography-line-height-body);
-  letter-spacing: var(--weave-typography-letter-spacing-normal);
+  font-size: var(--weave-typography-style-body-large-font-size);
+  font-weight: var(--weave-typography-style-body-large-font-weight);
+  line-height: var(--weave-typography-style-body-large-line-height);
+  letter-spacing: var(--weave-typography-style-body-large-letter-spacing);
   ${declarationBlock()}
 }
 
